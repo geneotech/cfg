08391447e0c871e5202f653e7398b7a8682809ea
Patryk Czachurski <patryk.czachurski@gmail.com>
2017-11-15 21:14:51 +0100

    Workspace selection implementation

---
+++ .config/i3/bind_select_workspace.sh
@@ -0,0 +1,8 @@
+WORKSPACES="
+/home/pbc/Hypersomnia
+/home/pbc/cfg
+"
+echo $WORKSPACES
+NEW_WORKSPACE=$(echo $WORKSPACES | sed 1d | rofi -hide-scrollbar -dmenu -i -p 'select workspace:')
+echo $NEW_WORKSPACE
+echo "export WORKSPACE=$NEW_WORKSPACE" > ~/.config/i3/workspace/current
