3b55a2f3711ca22999583ccdda5e244dfbbfdbf9
Patryk Czachurski <patryk.czachurski@gmail.com>
2017-11-16 06:44:43 +0100

    added proper cd for workspace's go to all

--- .config/i3/workspace/bind_workspace_find_all.sh
+++ .config/i3/workspace/bind_workspace_find_all.sh
@@ -4,11 +4,12 @@ LOCATION=$(find -L $WORKSPACE | sed 1d | rofi -hide-scrollbar -dmenu -i -p 'go t
 
 if [[ -d $LOCATION ]] 
 then
-  xterm -e "ranger $LOCATION"
+  terminator -e "ranger $LOCATION"
 else
-  RIFLE_RESULT=$(rifle $LOCATION)
+ cd $(dirname $LOCATION)
+ RIFLE_RESULT=$(rifle $LOCATION)
  if [[ ! -z $RIFLE_RESULT ]]
  then
-   xterm -e "ranger --selectfile=$LOCATION" 
+   terminator -e "ranger --selectfile=$LOCATION" 
  fi 
 fi
