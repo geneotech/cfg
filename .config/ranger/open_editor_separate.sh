18114347fb1a39504d586bc934c99c823a369f21
Patryk Czachurski <patryk.czachurski@gmail.com>
2017-11-17 15:22:54 +0100

    Don't open separate terminal for vim if we're in terminal

--- .config/ranger/open_editor_separate.sh
+++ .config/ranger/open_editor_separate.sh
@@ -1,1 +1,6 @@
-terminator -e "source ~/.config/ranger/open_editor.sh $1" &!
+if [[ $TERM -eq "linux" ]]
+then
+	terminator -e "source ~/.config/ranger/open_editor.sh $1" &!
+else
+	source ~/.config/ranger/open_editor.sh $1
+fi
