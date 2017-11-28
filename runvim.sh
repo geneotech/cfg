ddb62571224691dcd303112214722a4198d11796
Patryk Czachurski <patryk.czachurski@gmail.com>
2017-11-28 16:41:14 +0100

    tabs in vim and opening/focusing fully functional

--- runvim.sh
+++ runvim.sh
@@ -4,6 +4,7 @@ servlist=$(vim --serverlist)
 if [[ ! -z $servlist ]]
 then
     vim --remote-tab-silent $@
+	$(i3-msg "[title=VIM] focus")
 else
 	$($TERMINAL -e zsh -c "vim --servername VIM $@")
 fi
